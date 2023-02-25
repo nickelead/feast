d07be9f127357efd17318e2993e4e541e4a1bd01
nickelead <59025216+nickelead@users.noreply.github.com>
2023-02-25 03:11:27 +0300

    feat(app): Add colored boxes

--- App.js
+++ App.js
@@ -4,25 +4,57 @@ import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 const App = () => {
   return (
     <SafeAreaView style={styles.safeArea}>
-      <View style={[styles.container, styles.pink]}>
-        <Text>Hello, world!</Text>
+      <Text style={styles.heading}>
+        Here are some boxes of different colours
+      </Text>
+      <View style={[styles.container, styles.cyan]}>
+        <Text style={styles.text}>Cyan: #2aa198</Text>
+      </View>
+      <View style={[styles.container, styles.blue]}>
+        <Text style={styles.text}>Blue: #268bd2</Text>
+      </View>
+      <View style={[styles.container, styles.magenta]}>
+        <Text style={styles.text}>Magenta: #d33682</Text>
+      </View>
+      <View style={[styles.container, styles.orange]}>
+        <Text style={styles.text}>Orange: #cb4b16</Text>
       </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  safeArea: {
-    flex: 2,
-  },
-  pink: {
-    backgroundColor: 'pink',
-  },
   container: {
-    backgroundColor: 'teal',
-    alignItems: 'center',
+    padding: 10,
+    borderRadius: 3,
     justifyContent: 'center',
-    flex: 1,
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  heading: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  safeArea: {
+    paddingTop: 50,
+    paddingHorizontal: 10,
+  },
+  text: {
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  cyan: {
+    backgroundColor: '#2aa198',
+  },
+  blue: {
+    backgroundColor: '#268bd2',
+  },
+  magenta: {
+    backgroundColor: '#d33682',
+  },
+  orange: {
+    backgroundColor: '#cb4b16',
   },
 });
 
