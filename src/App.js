386f61e8cb123695d76b65170e05882816788374
nickelead <59025216+nickelead@users.noreply.github.com>
2023-02-25 16:13:55 +0300

    feat(home): Add routes for other colors

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Home from './screens/Home';
-import ColorPalette from './screens/ColorPalette';
+import { Home, ColorPalette } from './screens';
 
 const App = () => {
   const Stack = createStackNavigator();
@@ -12,7 +11,11 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="ColorPalette" component={ColorPalette} />
+        <Stack.Screen
+          name="ColorPalette"
+          component={ColorPalette}
+          options={({ route }) => ({ title: route.params.paletteName })}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
+import { SOLARIZED, FRONTEND_MASTERS, RAINBOW } from '../constants';
 
-const Home = ({ navigation }) => {
+export const Home = ({ navigation }) => {
   return (
     <View>
-      <TouchableOpacity onPress={() => navigation.navigate('ColorPalette')}>
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate('ColorPalette', {
+            colors: SOLARIZED,
+            paletteName: 'Solarized',
+          })
+        }>
         <Text>Solarized</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate('ColorPalette', {
+            colors: FRONTEND_MASTERS,
+            paletteName: 'Frontend Masters',
+          })
+        }>
+        <Text>Frontend Masters</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate('ColorPalette', {
+            colors: RAINBOW,
+            paletteName: 'Rainbow',
+          })
+        }>
+        <Text>Rainbow</Text>
       </TouchableOpacity>
     </View>
   );
 };
-
-export default Home;
